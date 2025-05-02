052dc9e044dc36eaccd098ba6cd39a89f1800cf2
RananjaSahy <sahyrananja2003@gmail.com>
2025-05-02 07:37:15 +0300

    Interceptor and conf Application

---
+++ ApiBreweryManagement/Infrastructure/Interceptors/EntityBaseAuditableInterceptor.cs
@@ -0,0 +1,42 @@
+﻿using ApiBreweryManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace ApiBreweryManagement.Infrastructure.Interceptors
+{
+    public class EntityBaseAuditableInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            UpdateTimestamps(eventData.Context);
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+            DbContextEventData eventData,
+            InterceptionResult<int> result,
+            CancellationToken cancellationToken = default)
+        {
+            UpdateTimestamps(eventData.Context);
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private void UpdateTimestamps(DbContext? context)
+        {
+            if (context == null) return;
+
+            foreach (var entry in context.ChangeTracker.Entries<EntityBaseAuditable>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.Created = DateTime.UtcNow;
+                    entry.Entity.LastModified = DateTime.UtcNow;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.LastModified = DateTime.UtcNow;
+                }
+            }
+        }
+    }
+}
--- ApiBreweryManagement/Program.cs
+++ ApiBreweryManagement/Program.cs
@@ -1,8 +1,18 @@
+using ApiBreweryManagement.Application.Common.Interfaces;
+using ApiBreweryManagement.Infrastructure.Interceptors;
+using ApiBreweryManagement.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddScoped<EntityBaseAuditableInterceptor>();
+builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>((serviceProvider, options) =>
+{
+    var interceptor = serviceProvider.GetRequiredService<EntityBaseAuditableInterceptor>();
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+           .AddInterceptors(interceptor);
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
