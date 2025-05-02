3db688317f0cf870e87845179a499b507df434f4
RananjaSahy <sahyrananja2003@gmail.com>
2025-05-02 07:39:13 +0300

    DbContext et Migration

---
+++ ApiBreweryManagement/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using ApiBreweryManagement.Application.Common.Interfaces;
+using ApiBreweryManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiBreweryManagement.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext() { }
+        public DbSet<Brewery> Breweries { get; set; }
+        public DbSet<Beer> Beers { get; set; }
+        public DbSet<Wholesaler> Wholesalers { get; set; }
+        public DbSet<WholesalerStock> WholesalerStocks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Beer>()
+                .HasOne(b => b.Brewery)
+                .WithMany(br => br.Beers)
+                .HasForeignKey(b => b.BreweryId);
+
+            modelBuilder.Entity<WholesalerStock>()
+                .HasOne(ws => ws.Wholesaler)
+                .WithMany(w => w.WholesalerStocks)
+                .HasForeignKey(ws => ws.WholesalerId);
+
+            modelBuilder.Entity<WholesalerStock>()
+                .HasOne(ws => ws.Beer)
+                .WithMany(b => b.WholesalerStocks)
+                .HasForeignKey(ws => ws.BeerId);
+        }
+
+    }
+}
---
+++ ApiBreweryManagement/Infrastructure/Persistence/Migrations/20250501190909_InitialCreate.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ApiBreweryManagement.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Breweries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    Created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Breweries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wholesalers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    Created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wholesalers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Beers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    AlcoholContent = table.Column<double>(type: "double precision", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    BreweryId = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Beers_Breweries_BreweryId",
+                        column: x => x.BreweryId,
+                        principalTable: "Breweries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WholesalerStocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    WholesalerId = table.Column<int>(type: "integer", nullable: false),
+                    BeerId = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WholesalerStocks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WholesalerStocks_Beers_BeerId",
+                        column: x => x.BeerId,
+                        principalTable: "Beers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WholesalerStocks_Wholesalers_WholesalerId",
+                        column: x => x.WholesalerId,
+                        principalTable: "Wholesalers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beers_BreweryId",
+                table: "Beers",
+                column: "BreweryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WholesalerStocks_BeerId",
+                table: "WholesalerStocks",
+                column: "BeerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WholesalerStocks_WholesalerId",
+                table: "WholesalerStocks",
+                column: "WholesalerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WholesalerStocks");
+
+            migrationBuilder.DropTable(
+                name: "Beers");
+
+            migrationBuilder.DropTable(
+                name: "Wholesalers");
+
+            migrationBuilder.DropTable(
+                name: "Breweries");
+        }
+    }
+}
