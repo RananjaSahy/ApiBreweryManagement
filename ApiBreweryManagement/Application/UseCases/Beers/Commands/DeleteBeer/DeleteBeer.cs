e13a6cceb754a05b22e39db17d11ed8f585e2e84
RananjaSahy <sahyrananja2003@gmail.com>
2025-05-02 07:38:00 +0300

    Beer Request

---
+++ ApiBreweryManagement/Application/UseCases/Beers/Commands/DeleteBeer/DeleteBeer.cs
@@ -0,0 +1,36 @@
+﻿using ApiBreweryManagement.Application.Common.Interfaces;
+
+namespace ApiBreweryManagement.Application.UseCases.Beers.Commands.DeleteBeer
+{
+    public class DeleteBeerCommand
+    {
+        public int BeerId { get; set; }
+        public int BreweryId { get; set; }
+    }
+
+    public class DeleteBeerHandler
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteBeerHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(DeleteBeerCommand command)
+        {
+            var brewery = await _context.Breweries.FindAsync(command.BreweryId);
+            if (brewery == null)
+            {
+                throw new Exception("Brasserie introuvable.");
+            }
+            var beer = await _context.Beers.FindAsync(command.BeerId);
+            if (beer == null)
+            {
+                throw new Exception("Bière introuvable.");
+            }
+            brewery.RemoveBeer(beer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ ApiBreweryManagement/Application/UseCases/Beers/Queries/GetBeersByBrewery.cs
@@ -0,0 +1,31 @@
+﻿using ApiBreweryManagement.Application.Common.Interfaces;
+using ApiBreweryManagement.Domain.Entities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace ApiBreweryManagement.Application.UseCases.Beers.Queries
+{
+    public class GetBeersByBreweryQuery
+    {
+        public int BreweryId { get; set; }
+    }
+
+    public class GetBeersByBreweryHandler
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetBeersByBreweryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Beer>> Handle(GetBeersByBreweryQuery query)
+        {
+            var brewery = await _context.Breweries.FindAsync(query.BreweryId);
+            if (brewery == null)
+            {
+                throw new Exception("Brasserie introuvable.");
+            }
+            return brewery.Beers.ToList();
+        }
+    }
+}
